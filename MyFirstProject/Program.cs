a3436cac457d221baeb43f67291696c807d0a9cb
Paula Kochańska <261681@pw.edu.pl>
2017-10-09 13:03:38 +0200

    First commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world!");
+
+            int a = 10;
+            string b = "label";
+            System.Console.WriteLine("Variables: {0}, {1}", a, b);
+
+            var c = "label2";
+
+            System.Console.WriteLine("\n");
+            System.Console.WriteLine("Value: {0}", c);
+            System.Console.WriteLine("Type: " + c.GetType());
+            System.Console.WriteLine("Length: " + c.Length);
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
